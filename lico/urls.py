53bcd547e2af69fc255f5f48cb15fa939cec45ec
JFAL2025 <josefernandoaruquipa1234@gmail.com>
2025-11-03 15:14:30 -0400

    Mi primer commit: descripción de los cambios

--- lico/urls.py
+++ lico/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.panel, name='panel'),
+    path('registro_v/', views.registro_v, name='registro_v'),
+    path('inventario/', views.inventario, name='inventario'),
+    path('reporte/', views.reporte, name='reporte'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+
 ]
--- lico/views.py
+++ lico/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def panel(request):
     return render(request, 'panel.html')
+
+def registro_v(request):
+    return render(request, 'registro_v.html')
+
+def inventario(request):
+    return render(request, 'inventario.html')
+def reporte(request):
+    return render(request, 'reporte.html')
+def catalogo(request):
+    return render(request, 'catalogo.html')
